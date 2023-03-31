ac8c2d16b06e4e73a9c73cdb902a8454eaf11f82
Dan Green <danngreen1@gmail.com>
2023-03-31 12:44:30 -0700

    qspi conf uses Board:: to determine pins D2 and D3

--- src/usb_dfu/norflash/qspi_flash_conf.hh
+++ src/usb_dfu/norflash/qspi_flash_conf.hh
@@ -1,11 +1,12 @@
 #pragma once
+#include "board_conf.hh"
 #include "norflash/qspi_flash_struct.hh"
 
 constexpr mdrivlib::QSPIFlashConfig qspi_flash_conf = {
 	.io0 = {GPIO::F, PinNum::_8, PinAF::AF_10},
 	.io1 = {GPIO::F, PinNum::_9, PinAF::AF_10},
-	.io2 = {GPIO::F, PinNum::_7, PinAF::AF_9},
-	.io3 = {GPIO::F, PinNum::_6, PinAF::AF_9},
+	.io2 = Board::NORFlash::d2,
+	.io3 = Board::NORFlash::d3,
 	.clk = {GPIO::F, PinNum::_10, PinAF::AF_9},
 	.cs = {GPIO::B, PinNum::_6, PinAF::AF_10},
 
