bc88176b831184352587098042e2a3d3f4e3e31b
Dan Green <danngreen1@gmail.com>
2023-04-04 07:14:27 -0700

    default to DK2 board

--- src/board_conf.hh
+++ src/board_conf.hh
@@ -6,18 +6,20 @@
 // -DBOARD_CONF_PATH="path/file.hh" or #define BOARD_CONF_PATH "path/file.hh" to use a custom board conf file.
 // Passing a compile flag -DBOARD_CONF_XXX is the preferred method, but you may also place the #define before any #include "board_conf.hh"
 
-#ifdef BOARD_CONF_PATH
 #define HEADER_STRING_I(s) #s
 #define HEADER_STRING(s) HEADER_STRING_I(s)
+
+#if defined(BOARD_CONF_PATH)
 #include HEADER_STRING(BOARD_CONF_PATH)
 
 #elif defined(BOARD_CONF_OSD32)
-#include "osd32brk_conf.hh"
+#include "board_conf/osd32brk_conf.hh"
 
 #elif defined(BOARD_CONF_DK2)
-#include "stm32disco_conf.hh"
+#include "board_conf/stm32disco_conf.hh"
 
 #else
-#error                                                                                                                 \
-	"You must select your board: #define BOARD_CONF_DK2 to select the MP153 Discovery Board; #define BOARD_CONF_OSD32 to select the OSD32 Board; or #define BOARD_CONF_PATH \"path/to/board_conf.hh\" to set the path to a custom board conf file. Passing a compile flag -DBOARD_CONF_XXX is the preferred method, but you may also place the #define before any #include \"board_conf.hh\""
+#include "board_conf/stm32disco_conf.hh"
+#warning                                                                                                                 \
+	"No board was selected, defaulting to STM32 MP157 Discovery board. See src/board_conf.hh" 
 #endif
