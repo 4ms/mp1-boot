8687cf33f11728dab048bc8f1533ca7e84f7d936
Dan Green <danngreen1@gmail.com>
2023-04-04 07:14:27 -0700

    default to DK2 board

--- src/board_conf.hh
+++ src/board_conf.hh
@@ -4,4 +4,20 @@
 // #include "osd32brk_conf.hh"
 // namespace Board = OSD32BRK;
 
-#include "stm32disco_conf.hh"
+#define HEADER_STRING_I(s) #s
+#define HEADER_STRING(s) HEADER_STRING_I(s)
+
+#if defined(BOARD_CONF_PATH)
+#include HEADER_STRING(BOARD_CONF_PATH)
+
+#elif defined(BOARD_CONF_OSD32)
+#include "board_conf/osd32brk_conf.hh"
+
+#elif defined(BOARD_CONF_DK2)
+#include "board_conf/stm32disco_conf.hh"
+
+#else
+#include "board_conf/stm32disco_conf.hh"
+#warning                                                                                                                 \
+	"No board was selected, defaulting to STM32 MP157 Discovery board. See src/board_conf.hh" 
+#endif
