b324ab12e5a06a043d230dd5b184bf84999caa4a
Dan Green <danngreen1@gmail.com>
2023-04-04 15:26:40 -0700

    Makefile allows for passing BOARD_CONF in build flags

--- src/board_conf.hh
+++ src/board_conf.hh
@@ -1,7 +1,23 @@
-// Uncomment one of these to select your board:
-// Or add your own at the end
+#pragma once
 
-// #include "osd32brk_conf.hh"
-// namespace Board = OSD32BRK;
+// You must select your board:
+// -DBOARD_CONF_DK2 or #define BOARD_CONF_DK2 to select the MP153 Discovery Board;
+// -DBOARD_CONF_OSD32 or #define BOARD_CONF_OSD32 to select the OSD32 Board;
+// -DBOARD_CONF_PATH="path/file.hh" or #define BOARD_CONF_PATH "path/file.hh" to use a custom board conf file.
+// Passing a compile flag -DBOARD_CONF_XXX is the preferred method, but you may also place the #define before any #include "board_conf.hh"
 
+#ifdef BOARD_CONF_PATH
+#define HEADER_STRING_I(s) #s
+#define HEADER_STRING(s) HEADER_STRING_I(s)
+#include HEADER_STRING(BOARD_CONF_PATH)
+
+#elif defined(BOARD_CONF_OSD32)
+#include "osd32brk_conf.hh"
+
+#elif defined(BOARD_CONF_DK2)
 #include "stm32disco_conf.hh"
+
+#else
+#error                                                                                                                 \
+	"You must select your board: #define BOARD_CONF_DK2 to select the MP153 Discovery Board; #define BOARD_CONF_OSD32 to select the OSD32 Board; or #define BOARD_CONF_PATH \"path/to/board_conf.hh\" to set the path to a custom board conf file. Passing a compile flag -DBOARD_CONF_XXX is the preferred method, but you may also place the #define before any #include \"board_conf.hh\""
+#endif
