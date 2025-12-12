8b4f3b431f4b0257bfd10745ac08191c6cd95d87
Dan Green <danngreen1@gmail.com>
2025-12-11 22:34:27 -0800

    stub out system security init

--- src/drivers/mp13x/systeminit.c
+++ src/drivers/mp13x/systeminit.c
@@ -38,40 +38,40 @@ void security_init()
 	////////////////////////////////////////////
 
 	// Disable protection to RCC->BDCR, PWR->CR2, RTC, backup registers
-	PWR->CR1 = PWR->CR1 | PWR_CR1_DBP;
-	while (!(PWR->CR1 & PWR_CR1_DBP))
-		;
+	// PWR->CR1 = PWR->CR1 | PWR_CR1_DBP;
+	// while (!(PWR->CR1 & PWR_CR1_DBP))
+	// 	;
 
-	// Disable Trust Zone
-	RCC->TZCR = 0;
+	// // Disable Trust Zone
+	// RCC->TZCR = 0;
 
-	// Allow read/write for all securable peripherals (top 6 bits are reserved)
-	TZPC->DECPROT0 = 0x03FFFFFF;
+	// // Allow read/write for all securable peripherals (top 6 bits are reserved)
+	// TZPC->DECPROT0 = 0x03FFFFFF;
 
-	// Allow read/write for SYSRAM and RETRAM
-	// TZPC->DECPROT5 = TZPC->DECPROT5 | 0x3FF;
+	// // Allow read/write for SYSRAM and RETRAM
+	// // TZPC->DECPROT5 = TZPC->DECPROT5 | 0x3FF;
 
-	// Allow non-secure access to SYSRAM
-	TZPC->TZMA1_SIZE = 0;
+	// // Allow non-secure access to SYSRAM
+	// TZPC->TZMA1_SIZE = 0;
 
-	// Disable all tamper detection
-	TAMP->CR1 = 0;
+	// // Disable all tamper detection
+	// TAMP->CR1 = 0;
 
-	// Allow non-secure access to GPIOZ
-	RCC->MP_AHB5ENSETR = RCC_MP_AHB5ENSETR_GPIOZEN;
-	GPIOZ->SECCFGR = 0;
+	// // Allow non-secure access to GPIOZ
+	// RCC->MP_AHB5ENSETR = RCC_MP_AHB5ENSETR_GPIOZEN;
+	// GPIOZ->SECCFGR = 0;
 
-	// TZC AXI port 1 and 2 clocks enable
-	RCC->MP_APB5ENSETR = RCC_MP_APB5ENSETR_TZC1EN;
-	RCC->MP_APB5ENSETR = RCC_MP_APB5ENSETR_TZC2EN;
+	// // TZC AXI port 1 and 2 clocks enable
+	// RCC->MP_APB5ENSETR = RCC_MP_APB5ENSETR_TZC1EN;
+	// RCC->MP_APB5ENSETR = RCC_MP_APB5ENSETR_TZC2EN;
 
-	// Read/write enable on all nsaid
-	TZC->REG_ID_ACCESSO = 0xFFFFFFFF;
+	// // Read/write enable on all nsaid
+	// TZC->REG_ID_ACCESSO = 0xFFFFFFFF;
 
-	// bit 30 / 31 => Secure Global Enable : write/read
-	// bit 0 / 1 => Region Enable for filter 0/1
-	TZC->REG_ATTRIBUTESO = TZC->REG_ATTRIBUTESO | (1 << 0) | (1 << 1) | (1 << 30) | (1 << 31);
+	// // bit 30 / 31 => Secure Global Enable : write/read
+	// // bit 0 / 1 => Region Enable for filter 0/1
+	// TZC->REG_ATTRIBUTESO = TZC->REG_ATTRIBUTESO | (1 << 0) | (1 << 1) | (1 << 30) | (1 << 31);
 
-	// Enable Filter 0 and 1
-	TZC->GATE_KEEPER = TZC->GATE_KEEPER | (1 << 0) | (1 << 1);
+	// // Enable Filter 0 and 1
+	// TZC->GATE_KEEPER = TZC->GATE_KEEPER | (1 << 0) | (1 << 1);
 }
