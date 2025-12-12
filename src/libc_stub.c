565339a614c3b86e5ff987c1d5c77f8422875175
Dan Green <danngreen1@gmail.com>
2025-12-11 21:53:42 -0800

    Add libc stubs

--- src/libc_stub.c
+++ src/libc_stub.c
@@ -1,4 +1,21 @@
-void _init(void) {}
-void _fini(void) {}
-int __errno;
+__attribute__((used)) void _init(void) {}
+
+__attribute__((used)) void _fini(void) {}
+
+__attribute__((used)) void _exit(int x)
+{
+	while (1)
+		;
+}
+
+__attribute__((used)) int _kill(int x) { return -1; }
+
+__attribute__((used)) int _getpid() { return -1; }
+
+__attribute__((used)) int *__errno()
+{
+	static int errno = 0;
+	return &errno;
+}
+
 void *__dso_handle = (void *)&__dso_handle;
