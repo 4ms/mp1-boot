d66ddd1b314f827c8e152972091f2214f2232af9
Dan Green <danngreen1@gmail.com>
2025-12-12 22:37:42 -0800

    Adjust mp13 board conf

--- src/board_conf/brainboard-mp13_conf.hh
+++ src/board_conf/brainboard-mp13_conf.hh
@@ -1,38 +1,33 @@
+#pragma once
 #include "drivers/clocks.hh"
 #include "drivers/i2c_conf.hh"
 #include "drivers/leds.hh"
 
 namespace Board
 {
-using OrangeLED = Led<GPIO::H, PinNum::_7, LedActive::High>;
+using RedLED = Led<GPIO::D, PinNum::_8, LedActive::Low>;
+using GreenLED = Led<GPIO::B, PinNum::_3, LedActive::High>; // Debug2
+using RedLED2 = RedLED;
+using GreenLED2 = GreenLED;
 
-using BlueLED = Led<GPIO::D, PinNum::_11, LedActive::High>;
+using OrangeLED = RedLED2;
+using BlueLED = GreenLED;
 
-using GreenLED = Led<GPIO::A, PinNum::_14, LedActive::High>;
+constexpr PinConf BootSelectPin{GPIO::E, PinNum::_2}; // DFU boot button
+constexpr bool UseBootSelect = true;
 
-using RedLED = Led<GPIO::A, PinNum::_13, LedActive::High>;
-
-using RedLED2 = OrangeLED; // For compatibility with OSD32BRK board
-using GreenLED2 = BlueLED; // For compatibility with OSD32BRK board
-
-// Button USER2 (also activates RedLED)
-constexpr bool UseBootSelect = false;
-constexpr PinConf BootSelectPin{GPIO::A, PinNum::_13};
-
-// Freeze mode: halts booting after initializing everything
-// so you can load firmware via SWD/JTAG
-constexpr bool UseFreezePin = false;
-constexpr PinConf FreezePin{GPIO::Unused, PinNum::_0};
+constexpr bool UseFreezePin = true;
+constexpr PinConf FreezePin{GPIO::B, PinNum::_4}; // DEBUG3 pin
 
 constexpr uint32_t ConsoleUART = UART4_BASE;
-constexpr PinConf UartRX{GPIO::B, PinNum::_2, PinAF::AF_8};
-constexpr PinConf UartTX{GPIO::G, PinNum::_11, PinAF::AF_6};
+constexpr PinConf UartRX{GPIO::A, PinNum::_15, PinAF::AF_8};
+constexpr PinConf UartTX{GPIO::A, PinNum::_9, PinAF::AF_8};
 
 namespace NORFlash
 {
-constexpr bool HasNORFlash = false;
-constexpr PinConf d2{};
-constexpr PinConf d3{};
+constexpr bool HasNORFlash = true;
+constexpr PinConf d2{GPIO::F, PinNum::_6, PinAF::AF_9};
+constexpr PinConf d3{GPIO::H, PinNum::_7, PinAF::AF_13};
 } // namespace NORFlash
 
 namespace PMIC
@@ -42,6 +37,7 @@ constexpr I2C_Config I2C_config{};
 } // namespace PMIC
 
 constexpr uint32_t HSE_Clock_Hz = 24000000;
-constexpr uint32_t MPU_MHz = 650;
-constexpr auto ClockType = SystemClocks::HSEClockSource::AnalogOsc;
+constexpr uint32_t MPU_MHz = 1000;
+constexpr auto ClockType = SystemClocks::HSEClockSource::Resonator;
+
 } // namespace Board
