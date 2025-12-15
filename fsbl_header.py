e0c6e780b89ec065fd9526e31e8b84494afc4c01
Dan Green <danngreen1@gmail.com>
2025-12-15 13:53:25 -0800

    Fix mp13x fsbl header script

--- fsbl_header.py
+++ fsbl_header.py
@@ -31,22 +31,24 @@ if arch == "stm32mp15x":
         0x00                                                                                # Binary type: U-Boot
         )
 else:
-    header = struct.pack("<ccccQQQQQQQQIIIIIIIIII20x384x",
+    header = struct.pack("<ccccQQQQQQQQIIIIIIIIIIIQQccBBBBBB376x",
         bytes('S', "ascii"), bytes('T', "ascii"), bytes('M', "ascii"), bytes('2', "ascii"), # cccc Header magic
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,                                     # QQQQQQQQ ECDSA signature, unsigned here
         sum(payload),                                                                       # I Checksum of payload, sum of all bytes
-        0x00020000,                                                                         # I Header version 1.0
+        0x00020000,                                                                         # I Header version 2.0
         len(payload),                                                                       # I Length of payload (without header)
-        0x2FFE0000 - 0x200,                                                                 # I Entrypoint address. SYSRAM - size of this header (0x200) so payload starts at SYSRAM start.
+        0x2FFE0000,                                                                         # I Entrypoint address. SYSRAM
         0x00000000,                                                                         # I Reserved (1/3)
         0x00000000,                                                                         # I Reserved (2/3)
         0x00000000,                                                                         # I Reserved (3/3)
-        0x00,                                                                               # I Image version
+        0x00000000,                                                                         # I Image version
         0x80000000,                                                                         # I Option flags, disable authentication and decryption, enable padding
-        0x00,                                                                               # I Header extensions length
-                                                                                            # 20B padding
-                         # 384B padding
-        # bytes('S', "ascii"), bytes('T', "ascii"), 0x00, 0x02,                               # Auth Header ext type
+        0x00000180,                                                                         # I Header extensions length
+        0x00000010,                                                                         # I 
+        0x00, 0x00,                                                                         # QQ 16B padding
+        bytes('S', "ascii"), bytes('T', "ascii"), 0xFF, 0xFF,                               # ccBB Auth Header ext type
+        0x80, 0x01, 0x00, 0x00,                                                             # BBBB
+                                                                                            # 376x padding
         )
 
 with open(sys.argv[2], "wb") as fsbl_file:
