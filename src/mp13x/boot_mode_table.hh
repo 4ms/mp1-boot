878c5e500ccbfed7545f510b9f3f723178385f2c
Dan Green <danngreen1@gmail.com>
2026-01-21 16:51:43 -0800

    Remove console debug

--- src/mp13x/boot_mode_table.hh
+++ src/mp13x/boot_mode_table.hh
@@ -6,15 +6,12 @@ inline uint32_t read_raw_bootrom_itf()
 	// Allocated by the linker, and value is written to the address by the startup script
 	extern uint32_t boot_mode_table_addr[];
 
-	print("&boot_mode_table_addr = 0x", Hex{(uint32_t)boot_mode_table_addr}, "\n");
-
 	if ((uint32_t)boot_mode_table_addr == 0) {
 		print("Boot mode table address not defined in linker script. Presuming SD Card boot\n");
 		return 1;
 	}
 
 	auto table_addr = boot_mode_table_addr[0];
-	print("boot_mode_table_addr = 0x", Hex{table_addr}, "\n");
 
 	if (table_addr == 0) {
 		print("Boot mode table was not set by startup.s. Presuming SD Card boot\n");
