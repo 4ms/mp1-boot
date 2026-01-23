570e52e5ff172812cf145f8783c371d66878792e
Dan Green <danngreen1@gmail.com>
2026-01-23 10:27:43 -0800

    Handle writing blocks that span multiple flash blocks

--- src/boot_sd_write_nor.cc
+++ src/boot_sd_write_nor.cc
@@ -31,11 +31,32 @@ bool NorFlashWriter::write(uint32_t nor_addr, std::span<const uint8_t> bytes)
 {
 	if (nor_addr < 0x7000'0000 || (nor_addr + bytes.size()) > 0x8000'0000) {
 		pr_err("Can only write to NOR Flash (0x70000000)\n");
+		return false;
+	}
+
+	if (bytes.size() > 4096) {
+		pr_err("Multi-block writes not implemented (max 4096B, requested ", bytes.size(), ")\n");
+		return false;
 	}
 
 	nor_addr -= 0x7000'0000;
 
 	auto &flash = flash_loader();
+
+	auto start_block = nor_addr >> 12;
+	auto end_block = (nor_addr + bytes.size() - 1) >> 12;
+	if (start_block != end_block) {
+		auto bytes_in_first_block = ((start_block + 1) << 12) - nor_addr;
+		debug("Write spans blocks. Writing to ", Hex{nor_addr}, " ", bytes_in_first_block, " bytes\n");
+		auto ok = flash.write(bytes.data(), nor_addr, bytes_in_first_block);
+		if (!ok) {
+			pr_err("Failed to write to flash address ", Hex{nor_addr}, "\n");
+			return false;
+		}
+
+		nor_addr = (start_block + 1) >> 12;
+		bytes = bytes.subspan(bytes_in_first_block);
+	}
 
 	if ((nor_addr & 0xFFF) == 0) {
 		log("Erasing 4k block at 0x", Hex{nor_addr}, "\n");
