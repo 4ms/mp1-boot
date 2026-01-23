98dcee7546d9fb9f9dd34540ceddb3d94e8e055b
Dan Green <danngreen1@gmail.com>
2026-01-23 07:22:02 -0800

    Erase 4k blocks before writing to the first page of each

--- src/boot_sd_write_nor.cc
+++ src/boot_sd_write_nor.cc
@@ -3,14 +3,14 @@
 #include "print.hh"
 #include "print_messages.hh"
 
-#include "flash_loader.hh"
+#include "qspi_flash_driver.hh"
 
 NorFlashWriter::NorFlashWriter() {}
 
 // Singleton, lazy-constructed
-FlashLoader &flash_loader()
+mdrivlib::QSpiFlash &flash_loader()
 {
-	static FlashLoader flash;
+	static mdrivlib::QSpiFlash flash;
 	return flash;
 }
 
@@ -29,37 +29,32 @@ static void dump_bytes(std::span<const uint8_t> bytes)
 
 bool NorFlashWriter::write(uint32_t nor_addr, std::span<const uint8_t> bytes)
 {
-	// Fix case where loader wants to write the uimg header.
-	// This is a problem because it could overwrite a previous image.
-	if (nor_addr == 0x7007'FFC0) { // app
-		bytes = bytes.subspan(0x40);
-		nor_addr = 0x7008'0000;
-	}
-	if (nor_addr == 0x7004'FFC0) { // ssbl
-		bytes = bytes.subspan(0x40);
-		nor_addr = 0x7005'0000;
-	}
-	if (nor_addr == 0x7003'FFC0) { // fsbl2
-		bytes = bytes.subspan(0x40);
-		nor_addr = 0x7005'0000;
-	}
-	if (nor_addr == 0x6FFF'FFC0) { // fsbl1
-		bytes = bytes.subspan(0x40);
-		nor_addr = 0x7000'0000;
+	if (nor_addr < 0x7000'0000 || (nor_addr + bytes.size()) > 0x8000'0000) {
+		pr_err("Can only write to NOR Flash (0x70000000)\n");
 	}
 
-	if (nor_addr < 0x7000'0000) {
-		pr_err("Can only write to NOR Flash (0x70000000)\n");
+	nor_addr -= 0x7000'0000;
+
+	auto &flash = flash_loader();
+
+	if ((nor_addr & 0xFFF) == 0) {
+		log("Erasing 4k block at 0x", Hex{nor_addr}, "\n");
+
+		if (!flash.erase(4096, nor_addr)) {
+			pr_err("ERROR: Flash failed to erase block\n");
+			return false;
+		}
 	}
 
 	debug("Writing to ", Hex{nor_addr}, " ", bytes.size(), " bytes\n");
 
-	auto ok = flash_loader().write_sectors(nor_addr, bytes);
-	if (ok) {
-		debug("Wrote OK\n");
-	} else {
+	auto ok = flash.write(bytes.data(), nor_addr, bytes.size());
+
+	if (!ok) {
 		pr_err("Failed to write to flash address ", Hex{nor_addr}, "\n");
+		return false;
 	}
 
+	debug("Wrote OK\n");
 	return true;
 }
